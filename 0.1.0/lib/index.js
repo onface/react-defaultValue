a11ed2bcc153aaaeaa9436720fe052cec3f283ac
Nimo <nimo.jser@gmail.com>
2018-04-28 15:07:34 +0800

    onface/module auto-generated commit

---
+++ 0.1.0/lib/index.js
@@ -0,0 +1,30 @@
+import React , { Component } from "react"
+import syncState from "sync-state"
+module.exports = function reactDefaultValue (Component) {
+    return class DefaultValue extends Component {
+        constructor (props) {
+            super(props)
+            const self = this
+            self.state = {
+                form: {
+                    value: ''
+                }
+            }
+        }
+        render() {
+            const self = this
+            let extendProps = {}
+            if (typeof self.props.defaultValue !== 'undefined' && typeof self.props.value === 'undefined') {
+                extendProps = syncState(this, 'form')('value', {
+                    defaultValue: self.props.defaultValue,
+                    sync: function (data) {
+                        return data
+                    }
+                })
+            }
+            return (
+                <Component ref="root" {...extendProps} {...self.props} />
+            )
+        }
+    }
+}
