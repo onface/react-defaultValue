a88fd3fe72996af727c8f32436da02171da72aa0
Nimo <nimo.jser@gmail.com>
2018-05-27 15:14:36 +0800

    test: menu output: "onPick"

---
+++ test/Menu.js
@@ -0,0 +1,30 @@
+import React , { Component } from "react"
+class Menu extends Component {
+    render() {
+        const self = this
+        return (
+            <div>
+                <strong>{self.props.value}</strong>
+                {
+                    self.props.list.map(function (value) {
+                        return (
+                            <button
+                                key={value}
+                                onClick={function() {
+                                    self.props.onPick(value)
+                                }}
+                            >{value}</button>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+Menu.defaultProps = {
+    list: ['meat', 'juice', 'egg'],
+    onPick:function (){}
+}
+// 一行代码实现 defaultValue
+Menu = require('../lib/index')(Menu, {output: 'onPick'})
+module.exports = Menu
---
+++ test/menu.test.js
@@ -0,0 +1,58 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import React, { Component } from "react"
+import Menu from "./Menu"
+
+class ControlledMenu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { value: 'egg' }
+    }
+    render () {
+        const self = this
+        return (
+            <Menu
+                value={self.state.value}
+                onPick={function (value) {
+                    self.setState({
+                        value: value
+                    })
+                }}
+            />
+        )
+    }
+}
+
+describe('<Menu />', () => {
+    it('defaultValue', () => {
+        var root = mount(<ControlledMenu />)
+        expect(
+            root.html()
+        ).to.equal('<div><strong>egg</strong><button>meat</button><button>juice</button><button>egg</button></div>')
+        root.find('button').at(0).simulate('click');
+        expect(
+            root.html()
+        ).to.equal('<div><strong>meat</strong><button>meat</button><button>juice</button><button>egg</button></div>')
+        root.find('button').at(1).simulate('click');
+        expect(
+            root.html()
+        ).to.equal('<div><strong>juice</strong><button>meat</button><button>juice</button><button>egg</button></div>')
+    })
+    it('controlled', () => {
+        var root = mount(<Menu defaultValue="egg" />)
+        expect(
+            root.html()
+        ).to.equal('<div><strong>egg</strong><button>meat</button><button>juice</button><button>egg</button></div>')
+        root.find('button').at(0).simulate('click');
+        expect(
+            root.html()
+        ).to.equal('<div><strong>meat</strong><button>meat</button><button>juice</button><button>egg</button></div>')
+        root.find('button').at(1).simulate('click');
+        expect(
+            root.html()
+        ).to.equal('<div><strong>juice</strong><button>meat</button><button>juice</button><button>egg</button></div>')
+    })
+})
